69341db969e4d5982baef4dd555704f553e5f347
Jesse Carter <jctr073@gmail.com>
2015-04-24 21:28:49 -0700

    removed non-used code

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,40 +5,7 @@ class EventsController < ApplicationController
 
   def index
   end
-
-  def old_search
-    # words = params[:search][:keywords] || ""
-
-    clt = EventbriteClient.new({ access_token: '2GT6L3NCHSCTQQREGLKO'})
-    resp = clt.event_search(region: "CA", keywords: "wine")
-
-    respond_to do |format|
-      format.html { render html: resp }
-      format.json { render json: resp }
-    end
-  end
-
-  def search_v2
-    base = 'https://www.eventbriteapi.com/v3/'
-    mthd = 'events/search/'
-
-    if params[:search][:keywords] != ""
-      kywrd = "q=#{params[:search][:keywords]}&"
-    else
-      kywrd = ""
-    end
-
-    qry  = "?#{kywrd}venue.city=San+Francisco&venue.region=CA&"
-    p(qry)
-    token = 'token=2GT6L3NCHSCTQQREGLKO'
-    api_url  = base + mthd + qry + token
-    resp = HTTParty.get(api_url)
-
-    respond_to do |format|
-      format.json { render json: resp }
-    end
-  end
-
+  
   def search
     p params
     eb = EventbriteV3.new
