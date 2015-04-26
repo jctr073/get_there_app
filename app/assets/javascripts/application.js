d156bc7931ecc7629892fd278bdb51b3cd31702a
Jesse Carter <jctr073@gmail.com>
2015-04-26 10:13:57 -0700

    Moving js code into application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,167 @@
 //= require turbolinks
 //= require_tree .
 
+$(document).ready(function () {
+    $wel = $('#welcome');
+    $elm = $('#event-list');
+    $map = $('#map-canvas');
+
+    $('#frmSearch').keypress(function (event) {
+        if (event.which == 13) {
+            $('#search').trigger('click');
+        }
+    });
+
+    $('#clear').click(function() {  //adding reload functionality to the "new search" button
+        window.location.reload();
+    });
+
+    $('#search').click(function(){
+
+        console.log(testFn());
+        $wel.addClass("hidden");
+        $.ajax({
+            type: "POST",
+            url: "/search.json",
+            dataType: "json",
+            data: { search: {
+                keywords: $('#keywords').val(),
+                city: $('#city').val(),
+                state: $('#state').val(),
+                fromDt: $('#fromDt').val(),
+                toDt: $('#toDt').val()
+            }
+            },
+            success: function (resp) {
+                console.log(resp);
+                if (resp.pagination.object_count > 0) {
+                    // Setup map
+                    var event1 = resp.events[0];
+                    console.log(event1);
+                    var mapOptions = {
+                        center: { lat: Number(event1.venue.latitude), lng: Number(event1.venue.longitude)},
+                        zoom: 13
+                    };
+                    var map = new google.maps.Map(document.getElementById('map-canvas'),
+                        mapOptions);
+
+                    loadEvents(resp, map);
+                } else {
+                    noResults();
+                }
+
+            }
+        });
+    })
+
+});
+
+function noResults () {
+    $elm.empty();
+    $map.empty();
+    $elm.html("No results for search.");
+}
+function loadEvents (resp, map) {
+    $elm.empty();
+    var events = resp.events;
+    for (var i = 0; i < events.length; i++) {
+        curEvent = events[i];
+        writeEventListing( i, $elm[0], curEvent );
+        plotMapPoints( i, map, curEvent );
+    }
+}
+
+function writeEventListing(itr, domElm, curEvent) {
+    itr++;
+    //Create the elements
+    var doc     = window.document;
+    var media   = doc.createElement("div");
+    var a       = doc.createElement("a");
+    var mbody   = doc.createElement("div");
+    var h5      = doc.createElement("h5");
+
+    //assign bootstrap classes
+    media.className = "media";
+    a.className     = "pull-left";
+    mbody.className = "media-body";
+    h5.className    = "media-heading";
+    h5.innerText    = itr + ": " + curEvent.name.text;
+
+    //assemble elements
+    domElm.appendChild(media);
+    media.appendChild(a);
+    if (curEvent.logo != null) {
+        var img       = doc.createElement("img");
+        img.src       = curEvent.logo.url;
+        img.className = "media-object img-thmb";
+        a.appendChild(img);
+    }
+    media.appendChild(mbody);
+    mbody.appendChild(h5);
+    var cat    = (curEvent.category != null)? curEvent.category.name : "";
+    var subCat = (curEvent.subcategory != null)? "/" + curEvent.subcategory.name + "<br>" : "";
+    $(mbody).append(cat + subCat);
+    $(mbody).append("Start: " + formatDate12HR(curEvent.start.local) + "<br>");
+    $(mbody).append("End:   " + formatDate12HR(curEvent.end.local));
+}
+
+function plotMapPoints(itr, gmap, curEvent) {
+
+    //variables for markers
+    itr++;
+    var icon1 = 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld='; //building icon
+    var icon2 = '|FF0000|000000'; //building icon
+    var lat = curEvent.venue.latitude;
+    var lng = curEvent.venue.longitude;
+    var title = curEvent.title;
+    // console.log($map);
+
+    var veName = curEvent.venue.name;
+    var veAddr = curEvent.venue.address.address_1;
+    var veCity = curEvent.venue.address.city;
+    var ev_sd  = formatDate12HR(curEvent.start.local);
+    var ev_ed  = formatDate12HR(curEvent.end.local);
+    var ev_url = curEvent.url;
+
+    var t;
+
+    var nextMkr = new google.maps.Marker({
+        position: new google.maps.LatLng(lat, lng),
+        map: gmap,
+        icon: icon1+itr+icon2, //creating unique markers dynamically
+        title: title
+    });
+
+    var infoWindowOptions = {
+        content: '<div id="infodiv">'+ veName+"<br />"+veAddr+"<br />"+veCity+"<br />"+
+        'Starts: '+ev_sd+"<br />"+'Ends: '+ev_ed + "<br />"+
+        '<a href="'+ ev_url +'" target="_blank">more info</a>'+'</div>' //modifying external url functionality
+    };
+
+    var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+    google.maps.event.addListener(nextMkr, 'mouseover', function () {
+        infoWindow.open(gmap, nextMkr);
+    });
+
+    google.maps.event.addListener(nextMkr, 'mouseout', function () {
+        t = setTimeout(function () {
+            infoWindow.close()
+        }, 1000);
+    });
+
+    google.maps.event.addListener(infoWindow, 'domready', function () {
+        $('#infodiv').on('mouseenter', function () {
+            clearTimeout(t);
+        }).on('mouseleave', function () {
+            t = setTimeout(function () {
+                infoWindow.close()
+            }, 1000);
+        });
+    });
+
+}
+
+
 
         
